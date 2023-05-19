299b7c160ab1bd17c89b0cabc553943eb4085eaa
haz10em <133292426+haz10em@users.noreply.github.com>
2023-05-19 17:12:02 +0300

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <fstream>
+using namespace std;
+ enum hazm(hazem,jf);
+int main()
+{
+	ofstream hazem;
+	hazem.open("YOur file.txt");
+	hazem<<"Hello Hazem ";
+	hazem.close();
+	
+
+ 
+}
